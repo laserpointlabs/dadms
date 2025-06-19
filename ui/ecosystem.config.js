8f04b0dafafdff3c57753283565eca4a100a691f
laserpointlabs <laserpointlabs@gmail.com>
2025-06-19 05:09:26 -0400

    feat: Add API endpoint for fetching BPMN XML of process definitions and implement proxy middleware for API calls

--- ui/ecosystem.config.js
+++ ui/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
             script: 'cli-api-server.js',
             cwd: '/home/jdehart/dadm/ui',
             instances: 1,
+            exec_mode: 'fork',
             autorestart: true,
             watch: false,
             max_memory_restart: '1G',
